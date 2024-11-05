6fb64d51a5ca8baf13a8b1aa43a05f93ab161163
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-11-04 22:02:35 -0500

    Configure RabbitMQ for JSON Message Communication

--- src/main/java/com/venkatesh/springboot/config/RabbitMQConfig.java
+++ src/main/java/com/venkatesh/springboot/config/RabbitMQConfig.java
@@ -1,9 +1,10 @@
 package com.venkatesh.springboot.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,11 +18,21 @@ public class RabbitMQConfig {
     private String exchange;
     @Value("${rabbitmq.routing.key}")
     private String routingKey;
+    @Value("${rabbitmq.queue.json.name}")
+    private String jsonQueue;
+    @Value("${rabbitmq.routing.json.key}")
+    private String jsonRoutingKey;
 
     // spring bean for rabbitmq queue
     @Bean
     public Queue queue(){
         return new Queue(queue);
+    }
+
+    // spring bean for queue (store json message)
+    @Bean
+    public Queue jsonQueue(){
+        return new Queue(jsonQueue);
     }
 
     //spring bean for rabbitmq exchange
@@ -37,6 +48,25 @@ public class RabbitMQConfig {
                 .bind(queue())
                 .to(exchange())
                 .with(routingKey);
+    }
+
+    // binding between json queue and exchange(same) using routing key
+    @Bean
+    public Binding jsonBinding(){
+        return BindingBuilder
+                .bind(jsonQueue())
+                .to(exchange())
+                .with(jsonRoutingKey);
+    }
+    @Bean
+    public MessageConverter converter(){
+        return new Jackson2JsonMessageConverter();
+    }
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate=new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
     }
 
     //spring boot autoconfiguration automatically configures the below infrastructure beans
---
+++ src/main/java/com/venkatesh/springboot/dto/User.java
@@ -0,0 +1,11 @@
+package com.venkatesh.springboot.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}

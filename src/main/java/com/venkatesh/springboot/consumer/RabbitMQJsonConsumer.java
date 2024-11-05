4090cd7da3aad21373cd389448ab713d30d5a1f5
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-11-05 06:22:02 -0500

    RabbitMQ JSON Consumer created

---
+++ src/main/java/com/venkatesh/springboot/consumer/RabbitMQJsonConsumer.java
@@ -0,0 +1,18 @@
+package com.venkatesh.springboot.consumer;
+
+import com.venkatesh.springboot.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQJsonConsumer {
+    private static final Logger LOGGER= LoggerFactory.getLogger(RabbitMQJsonConsumer.class);
+    @RabbitListener(queues = "${rabbitmq.queue.json.name}")
+    public void consumeJsonMessage(User user){
+        LOGGER.info(String.format("Received JSON message -> %s",user));
+
+    }
+}
